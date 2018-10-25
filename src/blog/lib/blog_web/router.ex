3ac77cb0734537e69847de47308de59984078cdf
Julian Wilke <julian.wilke@webteam-leipzig.de>
2018-10-25 14:33:38 +0200

    add comments boilerplate

--- src/blog/lib/blog_web/router.ex
+++ src/blog/lib/blog_web/router.ex
@@ -10,5 +10,6 @@ defmodule BlogWeb.Router do
 
     resources "/users", UserController, except: [:new, :edit]
     resources "/posts", PostController, except: [:new, :edit]
+    resources "/comments", CommentController, except: [:new, :edit]
   end
 end
