b91fd469d5028258c33f41ba4fb6f89670d71823
Julian Wilke <julian.wilke@webteam-leipzig.de>
2018-10-25 16:55:46 +0200

    POST /api/users: expect top level params instead of user object

--- src/blog/lib/blog_web/controllers/user_controller.ex
+++ src/blog/lib/blog_web/controllers/user_controller.ex
@@ -11,7 +11,7 @@ defmodule BlogWeb.UserController do
     render(conn, "index.json", users: users)
   end
 
-  def create(conn, %{"user" => user_params}) do
+  def create(conn, user_params) do
     with {:ok, %User{} = user} <- Blogs.create_user(user_params) do
       conn
       |> put_status(:created)
