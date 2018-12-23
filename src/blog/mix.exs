6dd59d3149a8c069d9f700075c4d2d817df7c5b2
Julian Wilke <julian.wilke@webteam-leipzig.de>
2018-12-23 23:05:39 +0100

    add credo for static code analysis

--- src/blog/mix.exs
+++ src/blog/mix.exs
@@ -39,7 +39,8 @@ defmodule Blog.Mixfile do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:plug_cowboy, "~> 1.0"}
+      {:plug_cowboy, "~> 1.0"},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
     ]
   end
 
